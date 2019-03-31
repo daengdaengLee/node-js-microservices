530cf62b791a62b5f73554c2a93102fab80356fb
daengdaengLee <gunho1020@gmail.com>
2019-03-31 22:35:19 +0900

    ENH: chap02/sec-express/exam02

--- chap02/sec-express/app.js
+++ chap02/sec-express/app.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const app = express();
+const stack = [];
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.post("/stack", (req, res, next) => {
+  let buffer = "";
+
+  req.on("data", data => {
+    buffer += data;
+  });
+  req.on("end", () => {
+    stack.push(buffer);
+    next();
+  });
+});
+
+app.delete("/stack", (req, res, next) => {
+  stack.pop();
+  next();
+});
+
+app.get("/stack/:index", (req, res) => {
+  if (req.params.index >= 0 && req.params.index < stack.length) {
+    res.send("" + stack[req.params.index]);
+    return;
+  }
+
+  res.status(404).end();
+});
+
+app.use("/stack", (req, res) => {
+  res.send(stack);
 });
 
 app.listen(3000);
